2ae10b07b1ad7eb3a8a09cc0b05fb66958592ac9
Brian Moon <brian@moonspot.net>
2024-09-05 14:45:06 -0500

    Add tmpdir helper

---
+++ src/TmpDir.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DealNews\TestHelpers;
+
+/**
+ * Creates a temporary directory with a random name and returns it
+ *
+ * @author      Brian Moon <brianm@dealnews.com>
+ * @copyright   1997-Present DealNews.com, Inc
+ * @package     DealNews\TestHelpers
+ */
+trait TmpDir {
+
+    public function tmpDir(?string $base_dir = null): string {
+
+        $base_dir ??= sys_get_temp_dir();
+
+        do {
+            $success = false;
+            $dir = $base_dir . '/' . base64_encode(random_bytes(32));
+            if (!file_exists($dir)) {
+                $success = mkdir($dir);
+            }
+        } while (!$success);
+
+        return $dir;
+    }
+}
---
+++ tests/TmpDirTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DealNews\TestHelpers\Tests;
+
+use \DealNews\TestHelpers\TmpDir;
+
+class TmpDirTest extends \PHPUnit\Framework\TestCase {
+
+    use TmpDir;
+
+    public function testTmpDir() {
+        $dir = $this->tmpDir();
+
+        $this->assertTrue(is_dir($dir));
+
+        $dir2 = $this->tmpDir($dir);
+
+        $this->assertTrue(is_dir($dir));
+
+        rmdir($dir2);
+        rmdir($dir);
+    }
+}
